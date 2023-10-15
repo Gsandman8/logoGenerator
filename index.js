0e9619d30e7336130bd2d23c6196fc9b44fffe52
Gsandman8 <gage.e.sandman@gmail.com>
2023-10-14 17:09:01 -0700

    Added base svg text, render functions for each shape , and additional tests for each shape

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
-
+const fs = require("fs")
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
 
 const questions = [
@@ -27,4 +27,18 @@ const questions = [
         type: "input",
         message: "Enter the color would you like your background to be? Please enter a hexadecimal value."
     }
-]
+];
+
+inquirer.prompt(questions).then(response => {
+    fs.writeFile("logo.svg", 
+`<svg version="1.1"
+width="300" height="200"
+xmlns="http://www.w3.org/2000/svg">
+
+${shape}
+
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.shapeColor}">${response.text}</text>
+
+</svg>
+`)
+})
